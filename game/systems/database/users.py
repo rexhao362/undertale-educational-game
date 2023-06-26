b7d0e66475697c7dd47ae031233e7bc726956612
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2023-06-26 02:50:01 +0100

    made user class

---
+++ game/systems/database/users.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.scores = {
+            'maths': {
+                'marks': 0,
+                'questions':0
+            }
+        }
+
+    def correct_answer(self, subject):
+        self.scores[subject]['marks'] += 1    
+        self.scores[subject]['questions'] += 1    
+    def wrong_answer(self, subject):
+        self.scores[subject]['questions'] += 1
+
+    def update_user(self):
+        pass    
