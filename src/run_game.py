938495ba101457201aac9dba2ac698ff7b2f2807
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2023-07-03 15:08:56 +0100

    save

--- src/run_game.py
+++ src/run_game.py
@@ -2,6 +2,7 @@ from src.systems.battle.player import Player
 from src.systems.battle.stage import Stage
 import pygame
 from src.main import screen
+from src.menus.main_menu import main_menu
 
 current_user = ['player']
 
@@ -24,8 +25,23 @@ class Game:
 
 def start_game():
     game = Game(screen, 'maths', current_user[0])
-    # main_menu.disable()
-    while game.stage < 5:
+    main_menu.disable()
+    running = True
+    while running:
+        events = pygame.event.get()
+        for e in events:
+            if e.type == pygame.QUIT:
+                exit()
+            elif e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_ESCAPE:
+                    main_menu.enable()
+
+                    # Quit this function, then skip to loop of main-menu on line 221
+                    return
+
+        # Pass events to main_menu
+        if main_menu.is_enabled():
+            main_menu.update(events)
         stage = game.next_stage()
         stage.turn_combat()
         
--- src/systems/battle/stage.py
+++ src/systems/battle/stage.py
@@ -8,20 +8,20 @@ class Stage:
         self.stage = current_stage
         self.enemy = create_enemy()
         self.screen.fill((255, 255, 255))
-        self.image = pygame.image.load('assets/pictures/backgrounds/boss_battle_bg.png')
-        self.bg_rect = self.image.get_rect()
+        self.bg_image = pygame.image.load('assets/pictures/backgrounds/boss_battle_bg.png')
+        self.bg_rect = self.bg_image.get_rect()
         self.bg_rect.center = (600, 400)
 
     def victory(self):
         if not self.enemy.is_alive():
             victory_img = 'assets/pictures/game_over.png'
-            self.screen.blit(victory_img)
+            # self.screen.blit(victory_img)
             self.game_won()
 
     def game_over(self):
         if not self.player.is_alive():
             defeat_img = 'assets/pictures/game_over.png'
-            self.screen.blit(defeat_img)
+            # self.screen.blit(defeat_img)
 
     def game_won(self):
         if self.stage == 5:
